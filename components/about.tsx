f394dcc931214a499b0f154d484674aae25387f1
Md Mohaiminul Islam <mi@netzlab.de>
2024-03-18 17:11:06 +0100

    fixed smooth scrolling and section transition

--- components/about.tsx
+++ components/about.tsx
@@ -4,10 +4,12 @@ import { motion } from "framer-motion"
 
 const About = () => {
     return (
-        <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+        <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40
+        scroll-mt-28"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.175 }} >
+            transition={{ delay: 0.175 }}
+            id="about" >
             <SectionHeading>About me</SectionHeading>
             <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aliquam odit quasi?
                 Rem, maiores eveniet dolores unde incidunt consectetur velit at officia ipsum ipsam
