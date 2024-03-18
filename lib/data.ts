1039def14d0286c3b263003bc8bc215b46f20fca
Md Mohaiminul Islam <mi@netzlab.de>
2024-03-18 16:54:11 +0100

    added projects section

--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,8 @@ import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+import expressversandImg from "@/public/expversand.png";
+import gardifyImg from "@/public/gardify.png";
 
 export const links = [
     {
@@ -81,6 +83,20 @@ export const projectsData = [
             "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
         tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
         imageUrl: wordanalyticsImg,
+    },
+    {
+        title: "Express Versand",
+        description:
+            "A public website for price comparison among different shipping service providers. I worked in this project as a Frontend Developer for 1 year. I have implemented features like autocomplete, pagination, sorting, filtering, secure checkout",
+        tags: ["React", "JavaScript", "Bootstrap", "CSS"],
+        imageUrl: expressversandImg,
+    },
+    {
+        title: "Gardify",
+        description:
+            "A public website for gardening and its maintenance. I worked in this project as a Frontend Developer for 6 months. It has features like autocomplete, dynamic page refresh, embedded YouTube video, sorting, filtering, secure checkout, ",
+        tags: ["Angular", "TypeScript", "Bootstrap", "SCSS"],
+        imageUrl: gardifyImg,
     },
 ] as const;
 
