5d73a7c9d8b86438ba12511ababc6a2cb176cbf1
Md Mohaiminul Islam <mi@netzlab.de>
2024-06-12 21:20:32 +0200

    new version

--- components/about.tsx
+++ components/about.tsx
@@ -15,9 +15,9 @@ const About = () => {
             transition={{ delay: 0.175 }}
             id="about" >
             <SectionHeading>About me</SectionHeading>
-            <p className="mb-3">In the big world of computers, I'm almost done with my Master's degree in Computer Science. I love making websites look awesome, and playing with CSS and JavaScript; of course React is my favorite tool.</p>
+            <p className="mb-3">In the big world of computers, I'm almost done with my Master's degree in Computer Science. I love making websites look awesome, playing with CSS and JavaScript; and making software an app more intuitive. Of course React/React Native is my favorite tool when it comes to building websites or app.</p>
 
-            <p>I spend a lot of time learning from Kevin Powell's videos and taking on design challenges from frontendmentor.io. When React said Next.js is cool, I jumped on board to learn it too.</p>
+            <p>I spend a lot of time learning from Kevin Powell's videos and taking on design challenges from frontendmentor.io. While staying updates about new trends on ASP .Net or Java; when React said Next.js is cool, I jumped on board to learn it too. I am also in the process of mastering Tailwind CSS and shadcn which is very helpful tool.</p>
 
             <p>Outside of coding, I'm a huge fan of football, tennis, and badminton. I also enjoy taking pictures of stars and nightscapes, and I like playing video games.</p>
 
